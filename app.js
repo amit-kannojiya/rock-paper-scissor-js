39169157cd016599712a07c6cc1eba9baaf4599f
amit-kannojiya <kannojiya.amit1028@gmail.com>
2023-12-20 17:07:18 -0800

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const userScore = 0;
-const compScore = 0;
+let userScore = 0;
+let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 const genCompChoice = () => {
   let options = ["rock", "paper", "scissor"];
   let randomIndex = Math.floor(Math.random() * 3);
   return options[randomIndex];
 };
 const draw = () => {
-  console.log("this game is draw");
+  msg.innerText = "this game is draw";
 };
+const showWinner = (userWin) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = "you won!";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    console.log("you lose");
+    msg.innerText = "you lose, try again";
+  }
+};
+
 const playGame = (userChoice) => {
   console.log("userchoice :", userChoice);
   const compChoice = genCompChoice();
@@ -17,6 +34,16 @@ const playGame = (userChoice) => {
 
   if (userChoice === compChoice) {
     draw();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      userWin = compChoice === "scissor" ? true : false;
+    } else {
+      compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin);
   }
 };
 
