4142735f3613bdd9c187d3dde154c0eff80cdbe7
amit-kannojiya <kannojiya.amit1028@gmail.com>
2023-12-20 16:12:23 -0800

    function creating

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const compScore = 0;
 const choices = document.querySelectorAll(".choice");
 const genCompChoice = () => {
   let options = ["rock", "paper", "scissor"];
-  let selectedChoice = Math.floor(Math.random() * 3);
+  let randomIndex = Math.floor(Math.random() * 3);
+  return options[randomIndex];
 };
-
+const draw = () => {
+  console.log("this game is draw");
+};
 const playGame = (userChoice) => {
   console.log("userchoice :", userChoice);
+  const compChoice = genCompChoice();
+  console.log("compChoice", compChoice);
+
+  if (userChoice === compChoice) {
+    draw();
+  }
 };
 
 choices.forEach((choice) => {
